eeab950ca911ec29b05484d0b410389430113e41
Njokom <knjokom@calstatela.edu>
2023-03-13 04:09:24 -0700

    prints a welcome message

--- boot.asm
+++ boot.asm
@@ -1,15 +1,24 @@
+[org 0x7c00]
 mov ah, 0x0e
-mov al, 'n'
-int 0x10
-mov ah, 0x0e
-mov al, 'O'
-int 0x10
-mov ah, 0x0e
-mov al, 'S'
-int 0x10
+mov bx, osWelcome
+
+printString:
+	mov al, [bx]
+	cmp al, 0
+	je end
+	int 0x10
+	inc bx
+	jmp printString
+end:
 
 
 jmp $
+
+osWelcome:
+	db "Welcome to Njokom Operating System", 0
+
+
+
 
 
 times 510-($-$$) db 0
