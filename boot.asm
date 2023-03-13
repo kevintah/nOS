6d83df880916aa6c3027074d8552dbd45176fa27
Njokom <knjokom@calstatela.edu>
2023-03-13 10:07:50 -0700

    It can accept and store one character through a keyboard, and then displays a welcome message.

--- boot.asm
+++ boot.asm
@@ -1,28 +1,43 @@
 [org 0x7c00]
-mov ah, 0x0e
-mov bx, osWelcome
 
-printString:
+
+
+
+readChar:
+	mov ah, 0
+	int 0x16
+
+printChar: 
+	mov ah, 0x0e
+	cmp al, 0
+	je end
+	int 0x10
+	
+	
+	
+mov bx, osWelcome
+	
+
+printString: 
+	mov ah, 0x0e
 	mov al, [bx]
 	cmp al, 0
 	je end
 	int 0x10
 	inc bx
 	jmp printString
+
 end:
 
 
 jmp $
 
 osWelcome:
-	db "Welcome to Njokom Operating System", 0
-
-
+	db 0ah, " Welcome to Njokom Operating System", 0
 
 
 
 times 510-($-$$) db 0
 
 db 0x55, 0xaa
-
 
